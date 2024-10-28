4593cb28656ff8a3e6a2f028dc4531a87a1598fc
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2024-10-28 14:45:19 -0300

    feat: policy page

---
+++ src/pages/politicas.tsx
@@ -0,0 +1,53 @@
+export default function Politica() {
+  return (
+    <div>
+      <h1>Política de Privacidade</h1>
+      <p>
+        Bem-vindo ao BSS Esportes! Valorizamos a privacidade de nossos usuários
+        e estamos comprometidos em protegê-la. Esta Política de Privacidade
+        explica como tratamos suas informações pessoais ao usar nosso site.
+      </p>
+
+      <h2>Informações Pessoais que Coletamos</h2>
+      <p>
+        Não coletamos nenhuma informação pessoal de nossos usuários. O acesso ao
+        nosso serviço é totalmente livre de registro de dados pessoais como
+        e-mail, senha ou qualquer outra informação.
+      </p>
+
+      <h2>Como Usamos Suas Informações Pessoais</h2>
+      <p>
+        Como não coletamos informações pessoais, não utilizamos nem processamos
+        quaisquer dados dos usuários.
+      </p>
+
+      <h2>Compartilhamento de Suas Informações Pessoais</h2>
+      <p>
+        Como não coletamos informações pessoais, não há dados a serem
+        compartilhados com terceiros.
+      </p>
+
+      <h2>Seus Direitos</h2>
+      <p>
+        Caso tenha dúvidas sobre nossa política de privacidade ou queira obter
+        mais informações, entre em contato conosco através do e-mail ou telefone
+        abaixo.
+      </p>
+
+      <h2>Alterações na Política de Privacidade</h2>
+      <p>
+        De tempos em tempos, podemos atualizar esta política para refletir
+        mudanças em nossas práticas de privacidade. Recomendamos que você reveja
+        esta página periodicamente para quaisquer mudanças.
+      </p>
+
+      <h2>Contato</h2>
+      <p>
+        Para mais informações sobre nossa política de privacidade, ou se você
+        tiver qualquer dúvida ou reclamação, entre em contato conosco pelo
+        e-mail brunoseverosartorio@hotmail.com ou pelo telefone +55 41
+        9727-1407.
+      </p>
+    </div>
+  );
+}
