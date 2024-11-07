376e9f3aabf01eede33dff8a3ff153d1c78e5b60
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2024-11-07 10:27:28 -0300

    feat: adjusts

--- src/pages/editGame/index.tsx
+++ src/pages/editGame/index.tsx
@@ -6,31 +6,14 @@ import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import styles from "./styles.module.css";
 
+import SearchSelectChampionship from "@/components/SearchSelectChampionship";
+import SearchSelectTeam from "@/components/SearchSelectTeam";
 import HomeButton from "../../components/HomeButton";
 
 interface PlayerDetail {
   id: string;
   name: string;
-  photo: string; // Ou 'logo', dependendo da propriedade real no seu objeto Item
-}
-
-interface Matche {
-  championship: string; // Mantém como string, ajuste conforme necessário
-  date: Date;
-  modality: string; // Mantém como string, ajuste conforme necessário
-  team_1: {
-    score: number;
-    team_id: string; // Considere mudar para um objeto ou ID se necessário
-  };
-  team_2: {
-    score: number;
-    team_id: string; // Considere mudar para um objeto ou ID se necessário
-  };
-  venue: string;
-  fase: string;
-  time: string;
-  topScorer: PlayerDetail | null;
-  mvp: PlayerDetail | null;
+  photo: string;
 }
 
 interface Item {
@@ -44,70 +27,56 @@ export default function EditMatch() {
   const { id } = router.query;
 
   const [matchData, setMatchData] = useState({
-    team1Name: "",
-    team1Logo: null as File | string | null,
     team1Score: 0,
-    team2Name: "",
-    team2Logo: null as File | string | null,
     team2Score: 0,
     date: "",
-    location: "",
+    venue: "",
     fase: "",
     fileURL: "",
     topScorer: "",
     mvp: "",
   });
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setMatchData((prevState) => ({ ...prevState, [name]: value }));
-  };
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files && event.target.files[0];
-    // @ts-ignore
-    setMatchData((prevState) => ({
-      ...prevState,
-      fileURL: file || prevState.fileURL,
-    }));
-  };
+  const [selectedTeamOne, setSelectedTeamOne] = useState<Item | null>(null);
+  const [selectedTeamTwo, setSelectedTeamTwo] = useState<Item | null>(null);
+  const [selectedChampionship, setSelectedChampionship] = useState<Item | null>(
+    null
+  );
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   useEffect(() => {
     const fetchMatch = async () => {
-      if (!id) {
-        console.log("Match ID is not defined.");
-        return;
-      }
+      if (!id) return;
 
       try {
         const matchDoc = await getDoc(doc(db, "matches", id as string));
         if (matchDoc.exists()) {
           const match = matchDoc.data();
           if (match) {
-            const team1Ref = match.team_1.team_id;
-            const team2Ref = match.team_2.team_id;
-
-            const team1Doc = await getDoc(team1Ref);
-            const team2Doc = await getDoc(team2Ref);
-
-            // @ts-ignore
-            const team1Name = team1Doc.exists() ? team1Doc.data().name : "";
-            // @ts-ignore
-            const team2Name = team2Doc.exists() ? team2Doc.data().name : "";
-
             setMatchData({
-              team1Name: team1Name,
-              team1Logo: match.team_1 ? match.team_1.logo : null,
-              team1Score: match.team_1 ? match.team_1.score : 0,
-              team2Name: team2Name,
-              team2Logo: match.team_2 ? match.team_2.logo : null,
-              team2Score: match.team_2 ? match.team_2.score : 0,
+              team1Score: match.team_1?.score || "",
+              team2Score: match.team_2?.score || "",
               date: match.date || "",
-              location: match.venue || "",
+              venue: match.venue || "",
               fase: match.fase || "",
               fileURL: match.fileURL || "",
               topScorer: match.topScorer || "",
               mvp: match.mvp || "",
+            });
+            setSelectedTeamOne({
+              id: match.team_1.team_id.id,
+              name: match.team_1.team_id.name,
+              logo: match.team_1.team_id.logo,
+            });
+            setSelectedTeamTwo({
+              id: match.team_2.team_id.id,
+              name: match.team_2.team_id.name,
+              logo: match.team_2.team_id.logo,
+            });
+            setSelectedChampionship({
+              id: match.championship.id,
+              name: match.championship.name,
+              logo: match.championship.logo,
             });
           }
         } else {
@@ -125,23 +94,17 @@ export default function EditMatch() {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     if (!id) {
-      console.error("Error: Match ID is not defined");
-      toast.error("Error when updating match.");
+      toast.error("Erro ao atualizar a partida.");
       return;
     }
 
     try {
       let fileURL = matchData.fileURL;
 
-      // @ts-ignore
-      if (matchData.fileURL instanceof File) {
-        const storageRef = ref(
-          storage,
-          `matches/${id}/${matchData.fileURL.name}`
-        );
-        await uploadBytes(storageRef, matchData.fileURL);
+      if (selectedFile) {
+        const storageRef = ref(storage, `matches/${id}/${selectedFile.name}`);
+        await uploadBytes(storageRef, selectedFile);
         fileURL = await getDownloadURL(storageRef);
       }
 
@@ -149,37 +112,41 @@ export default function EditMatch() {
         ...matchData,
         team_1: {
           score: matchData.team1Score.toString(),
+          team_id: doc(db, "teams", selectedTeamOne?.id || ""),
         },
         team_2: {
           score: matchData.team2Score.toString(),
+          team_id: doc(db, "teams", selectedTeamTwo?.id || ""),
         },
+        championship: doc(db, "championships", selectedChampionship?.id || ""),
         fileURL,
       };
 
-      // @ts-ignore
-      delete matchDataToSave.team1Name;
-      // @ts-ignore
-      delete matchDataToSave.team1Logo;
-      // @ts-ignore
-      delete matchDataToSave.team1Score;
-      // @ts-ignore
-      delete matchDataToSave.team2Name;
-      // @ts-ignore
-      // @ts-ignore
-      delete matchDataToSave.team2Logo;
-      // @ts-ignore
-      delete matchDataToSave.team2Score;
-
-      // Atualiza o documento no Firestore
       await setDoc(doc(db, "matches", id as string), matchDataToSave, {
         merge: true,
       });
-
-      toast.success("Match updated successfully!");
+      toast.success("Partida atualizada com sucesso!");
     } catch (error) {
-      console.error("Error when updating match: ", error);
-      toast.error("Error when updating match.");
+      console.error("Erro ao atualizar a partida:", error);
+      toast.error("Erro ao atualizar a partida.");
     }
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setSelectedFile(file || null);
+  };
+
+  const handleSelectTeamOne = (item: Item) => {
+    setSelectedTeamOne(item);
+  };
+
+  const handleSelectTeamTwo = (item: Item) => {
+    setSelectedTeamTwo(item);
+  };
+
+  const handleSelectChampionship = (item: Item) => {
+    setSelectedChampionship(item);
   };
 
   function HandleBackButtonClick() {
@@ -198,31 +165,19 @@ export default function EditMatch() {
 
           <form onSubmit={handleSubmit}>
             <div className={styles.form}>
-              <p className={styles.label}>
-                Nome do Time 1: {matchData.team1Name}
-              </p>
-              <input
-                className={styles.field}
-                type="text"
-                name="team1Name"
-                value={matchData.team1Name}
-                onChange={handleInputChange}
-                disabled
+              <p className={styles.label}>Campeonato:</p>
+              <SearchSelectChampionship
+                onSelectItem={handleSelectChampionship}
               />
             </div>
 
             <div className={styles.form}>
-              <p className={styles.label}>
-                Nome do Time 2: {matchData.team2Name}
-              </p>
-              <input
-                className={styles.field}
-                type="text"
-                name="team2Name"
-                value={matchData.team2Name}
-                onChange={handleInputChange}
-                disabled
-              />
+              <p className={styles.label}>Time 1:</p>
+              <SearchSelectTeam onSelectItem={handleSelectTeamOne} />
+            </div>
+            <div className={styles.form}>
+              <p className={styles.label}>Time 2:</p>
+              <SearchSelectTeam onSelectItem={handleSelectTeamTwo} />
             </div>
 
             <div className={styles.form}>
@@ -232,7 +187,12 @@ export default function EditMatch() {
                 type="text"
                 name="topScorer"
                 value={matchData.topScorer}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({
+                    ...prev,
+                    topScorer: e.target.value,
+                  }))
+                }
               />
             </div>
 
@@ -243,7 +203,9 @@ export default function EditMatch() {
                 type="text"
                 name="mvp"
                 value={matchData.mvp}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({ ...prev, mvp: e.target.value }))
+                }
               />
             </div>
 
@@ -254,7 +216,12 @@ export default function EditMatch() {
                 type="number"
                 name="team1Score"
                 value={matchData.team1Score}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({
+                    ...prev,
+                    team1Score: Number(e.target.value),
+                  }))
+                }
               />
             </div>
 
@@ -265,7 +232,12 @@ export default function EditMatch() {
                 type="number"
                 name="team2Score"
                 value={matchData.team2Score}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({
+                    ...prev,
+                    team2Score: Number(e.target.value),
+                  }))
+                }
               />
             </div>
 
@@ -276,7 +248,9 @@ export default function EditMatch() {
                 type="date"
                 name="date"
                 value={matchData.date}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({ ...prev, date: e.target.value }))
+                }
               />
             </div>
 
@@ -285,9 +259,14 @@ export default function EditMatch() {
               <input
                 className={styles.field}
                 type="text"
-                name="location"
-                value={matchData.location}
-                onChange={handleInputChange}
+                name="venue"
+                value={matchData.venue || ""}
+                onChange={(e) =>
+                  setMatchData((prev) => ({
+                    ...prev,
+                    venue: e.target.value,
+                  }))
+                }
               />
             </div>
 
@@ -298,7 +277,9 @@ export default function EditMatch() {
                 type="text"
                 name="fase"
                 value={matchData.fase}
-                onChange={handleInputChange}
+                onChange={(e) =>
+                  setMatchData((prev) => ({ ...prev, fase: e.target.value }))
+                }
               />
             </div>
 
@@ -307,7 +288,6 @@ export default function EditMatch() {
               <input
                 className={styles.fieldFile}
                 type="file"
-                name="fileURL"
                 accept="application/pdf"
                 onChange={handleFileChange}
               />
