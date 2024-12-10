72375bf7e74cc0137a09365c1c6ed7510ebddbb2
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2024-12-10 14:01:07 -0300

    feat: suporte page

---
+++ src/pages/suporte.tsx
@@ -0,0 +1,69 @@
+export default function Suporte() {
+  return (
+    <div
+      style={{
+        fontFamily: "'Montserrat', sans-serif",
+        textAlign: "center",
+        padding: "20% 10%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        backgroundColor: "#fff",
+      }}
+    >
+      <h1>Bem-vindo ao Suporte do BSS Esportes!</h1>
+      <p>
+        Estamos aqui para ajudar você com qualquer dúvida ou problema que possa
+        surgir ao usar o BSS Esportes. Nossa equipe está comprometida em
+        oferecer a melhor experiência possível para você.
+      </p>
+
+      <h2>Como Podemos Ajudar?</h2>
+      <p>
+        Confira as seções abaixo para obter assistência imediata ou entre em
+        contato conosco para suporte personalizado.
+      </p>
+
+      <h2>Precisa de Ajuda Imediata?</h2>
+      <p>
+        Se você não encontrou o que procurava na nossa seção de perguntas
+        frequentes, entre em contato diretamente conosco. Nossa equipe está
+        pronta para resolver qualquer problema ou dúvida.
+      </p>
+
+      <h2>Entre em Contato</h2>
+      <p>
+        Para suporte técnico ou questões relacionadas ao uso do BSS Esportes,
+        entre em contato conosco pelos canais abaixo:
+      </p>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>
+          <strong>E-mail:</strong>{" "}
+          <a href="mailto:brunoseverosartorio@hotmail.com">
+            brunoseverosartorio@hotmail.com
+          </a>
+        </li>
+        <li>
+          <strong>Telefone:</strong> +55 41 9727-1407
+        </li>
+      </ul>
+
+      <h2>Agradecemos pelo seu Feedback</h2>
+      <p>
+        Sua opinião é muito importante para nós! Se você tiver sugestões para
+        melhorar o BSS Esportes, por favor, compartilhe conosco. Queremos tornar
+        o aplicativo ainda melhor para você.
+      </p>
+
+      <h2>Mantenha-se Atualizado</h2>
+      <p>
+        Fique de olho em nossas atualizações e novidades! Estamos sempre
+        trabalhando para trazer novos recursos e conteúdo para o BSS Esportes.
+      </p>
+
+      <p>
+        Obrigado por usar o BSS Esportes! Estamos à disposição para ajudá-lo.
+      </p>
+    </div>
+  );
+}
